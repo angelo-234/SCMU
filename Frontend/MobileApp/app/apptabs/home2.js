45b800e757c43a98822d6f68ca799df7bb000d0c
angelo-234 <angelobennet1234@gmail.com>
2023-06-08 14:54:40 +0100

    bathroom password popup

--- Frontend/MobileApp/app/apptabs/home2.js
+++ Frontend/MobileApp/app/apptabs/home2.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, Button, TouchableOpacity } from 'react-native';
+import { View, Text, Button, TouchableOpacity, Alert, Modal, Pressable } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import tw from 'twrnc';
 // context imports
@@ -8,6 +8,7 @@ import { BathroomContext } from './BathroomContext';
 
 export default function Home2() {
   const navigation = useNavigation();
+  const [modalVisible, setModalVisible] = useState(false);
 
   // context
   const { hasSelectedBathroom } = useContext(BathroomContext);
@@ -29,9 +30,35 @@ export default function Home2() {
     navigation.navigate('reserveSlot');
   };
 
+  const handleBathroomCode = () => {
+    setModalVisible(true);
+  };
+
   return (
     <View style={tw`flex-1 items-center justify-center p-4  bg-blue-100`}>
-      {(hasSelectedBathroom ) ? (
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          //Alert.alert('Modal has been closed.');
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={tw`flex-1 justify-center items-center`}>
+          <View style={tw`bg-white rounded-2xl p-16 shadow-lg`}>
+            <Text style={tw`mb-4 text-center text-xl`}>Bathroom Code:</Text>
+            <Text style={tw`mb-4 text-center text-lg`}>1234</Text>
+            <Pressable
+              style={tw`bg-blue-500 rounded-lg px-4 py-2`}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={tw`text-white font-bold text-center`}>Ok</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      {(hasSelectedBathroom) ? (
         <View style={tw`items-center`}>
           <Text style={tw`text-xl font-bold mb-2`}>{`Bathroom ${hasSelectedBathroom}`}</Text>
           {hasSelectedTimeslot && (
@@ -39,23 +66,31 @@ export default function Home2() {
           )}
           <View style={tw`flex-col justify-center w-full`}>
             {hasSelectedTimeslot && (
+              <View>
               <TouchableOpacity
-              style={tw`m-1 px-1 py-2 bg-blue-500 rounded`}
-              onPress={handleViewBathroomStatus}
-            >
-            <Text style={tw`text-white text-base font-bold text-center`}>Bathroom Status</Text>
-          </TouchableOpacity>
+                style={tw`m-1 px-1 py-2 bg-blue-500 rounded`}
+                onPress={handleViewBathroomStatus}
+              >
+                <Text style={tw`text-white text-base font-bold text-center`}>Bathroom Status</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={tw`m-1 px-1 py-2 bg-green-500 rounded`}
+                onPress={handleBathroomCode}
+              >
+                <Text style={tw`text-white text-base font-bold text-center`}>Bathroom Code</Text>
+              </TouchableOpacity>
+              </View>
             )}
             <TouchableOpacity
-                style={tw`m-1 px-1 py-2 bg-blue-500 rounded`}
-                onPress={handleCancelReservation}
-              >
+              style={tw`m-1 px-1 py-2 bg-blue-500 rounded`}
+              onPress={handleCancelReservation}
+            >
               <Text style={tw`text-white text-base font-bold text-center`}>Cancel Bathroom</Text>
             </TouchableOpacity>
             <TouchableOpacity
-                style={tw`m-1 px-1 py-2 bg-blue-500 rounded`}
-                onPress={handleSelectTimeslot}
-              >
+              style={tw`m-1 px-1 py-2 bg-blue-500 rounded`}
+              onPress={handleSelectTimeslot}
+            >
               <Text style={tw`text-white text-base font-bold text-center`}>Select Timeslot</Text>
             </TouchableOpacity>
           </View>
